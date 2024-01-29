6c08cdbdc47c6f8f0f9b80f2a9d5416133411494
young24n <won8560@gmail.com>
2024-01-29 15:29:24 +0900

    melon scrap 기능 구현

--- scrapFeats/scrapMelon.js
+++ scrapFeats/scrapMelon.js
@@ -1,21 +1,31 @@
 const puppeteer = require('puppeteer');
 const fs = require("fs");
+const { request } = require('http');
 
-async function run(){
-    const browser = await puppeteer.launch({
-        headless: "new"
-    });
+async function scrapMelon(url) {
+    const browser = await puppeteer.launch({headless: "new"});
     const page = await browser.newPage();
-   
-    await page.goto("input value");//인풋 벨류로 가져오고
+    await page.goto(url);
 
+    // 돔 요소 파악 후 스크랩 지정
+    const songs = await page.$$eval(".section_playlist tbody tr", (elements) => {
+        return elements.map((e) => ({
+            song: e.querySelector(".fc_gray").innerText,
+            artist: e.querySelector(".fc_mgray").innerText,
+        }));
+    });
+    console.log(songs);
 
-    fs.writeFile("playlist.json", JSON.stringify(), (err)=>{
-        if(err) throw err;
-        console.log("파일 저장완료")
-    })
-    //기본 순환 열었으면 닫아야함
+    // JSON 파일에 저장
+    fs.writeFile("playlist.json", JSON.stringify(songs), (err) => {
+        if (err) throw err;
+        console.log("파일 저장 완료");
+    });
+
+    // 기본 순환 열었으면 닫아야 함
     await browser.close();
 }
 
-run();
+scrapMelon("https://www.melon.com/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=530475094");
+
+// export default scrapMelon
