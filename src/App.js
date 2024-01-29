486087cf8c251deba3aa9bc18b6a1589dbd2ce86
young24n <won8560@gmail.com>
2024-01-29 15:48:19 +0900

    주석 추가 log->alert 변경

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ function App() {
       <h6 style="text-align: center; font-family: Gamja Flower; margin: 0px; border: 0px; color: #c6c3c3;">Melon, Genie 플랫폼만 사용가능</h6>
       <hr class="hr-1"/>
       <div>
-          <input type="text" onChange={(e)=>{
+          <input type="text" onChange={(e)=>{//url 입력
             seturl(e.target.value)
           }}/>
       </div>
-      <button class="button btnGenie btnOrange" onClick={(e)=>{
+      <button class="button btnGenie btnOrange" onClick={()=>{//클릭 시 url 판단
         if(url.includes('www.melon.com')){
           scrapMelon(url)
         }
         else if(url.includes('www.genie.com')){
           scrapGenie(url)
         }
-        else{console.log("지원하지 않는 URL입니다.(멜론, 지니뮤직만 지원)")}
+        else{alert("지원하지 않는 URL입니다.(멜론, 지니뮤직만 지원)")}
       }}/>
     </div>
   );
