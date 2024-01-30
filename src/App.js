e61101324e9cd693a15e9c2f325948298057b89a
young24n <won8560@gmail.com>
2024-01-30 09:45:40 +0900

    파일 구조 변경에 의한 import 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import './App.css';
-import scrapMelon from '../scrapFeats/scrapMelon'
+import scrapMelon from './scrapFeats/scrapMelon'
+import scrapGenie from './scrapFeats/scrapGenie';
+
 function App() {
-  const [url, seturl] = useState("")
+    const [url, seturl] = useState("")
   return (
     <div>
       <h6 style="text-align: center; font-family: Gamja Flower; margin-bottom: 0px;">YTMusic으로 변환할 재생목록의 URL을 입력해주세요</h6>
@@ -21,7 +23,9 @@ function App() {
           scrapGenie(url)
         }
         else{alert("지원하지 않는 URL입니다.(멜론, 지니뮤직만 지원)")}
-      }}/>
+
+        //youtube api 땡겨온 후 재생목록 생성함수 추가
+      }}></button>
     </div>
   );
 }
