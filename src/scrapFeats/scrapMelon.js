784f56a9b01362b87cad553a7420506bc2ece43c
young24n <won8560@gmail.com>
2024-02-17 22:07:06 +0900

    변수 올바르게 할당

--- src/scrapFeats/scrapMelon.js
+++ src/scrapFeats/scrapMelon.js
@@ -16,7 +16,7 @@ async function doPuppeteerThings(url) {
 
     // Puppeteer를 사용하여 원격연결
     const browser = await puppeteer.connect({
-        browserWSEndpoint: "ws://127.0.0.1:9222/devtools/browser/0611d9b5-958f-48a2-8aa5-7c7ac8ac168e",        
+        browserWSEndpoint: browserWSEndpoint,        
     });
 
     // Puppeteer로 원격 웹 페이지에서 작업 수행
@@ -32,15 +32,6 @@ async function doPuppeteerThings(url) {
 
     // Puppeteer 브라우저 닫기
     await browser.close();
-}
-
-// CORS 문제를 해결하기 위한 프록시 설정
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-
-// 원격 서버에 요청을 보내는 함수를 프록시를 통해 보내도록 수정
-async function sendRequest(url) {
-    const response = await fetch(proxyUrl + url);
-    return response;
 }
 
 export default doPuppeteerThings
